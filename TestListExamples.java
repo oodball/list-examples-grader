a8619c4f2f4b38db61d5d28eacdbcbbac0fd0b4d
oodball <74622107+oodball@users.noreply.github.com>
2022-11-28 01:33:58 -0800

    idk if i can finish this

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,93 @@
 import static org.junit.Assert.*;
 import org.junit.*;
+import java.util.*;
+import java.util.List;
+
+class Checker implements StringChecker {
+  public boolean checkString(String s) {
+    return s.contains("tired422");
+  }
+}
 
 public class TestListExamples {
-  // Write your grading tests here!
+  // Write your grading tests here
+
+  StringChecker strCheck = new Checker();
+
+  @Test(timeout = 100)
+  public void testFilter() {
+    List<String> input = Arrays.asList(new String[] { "tired42", "tired423", "tired422" });
+    List<String> output = Arrays.asList(new String[] { "tired422" });
+    List<String> s = ListExamples.filter(input, strCheck);
+
+    Assert.assertArrayEquals(output.toArray(), s.toArray());
+  }
+
+  @Test(timeout = 100)
+  public void testFilterNull() {
+    List<String> input = Arrays.asList(new String[] {});
+    List<String> output = Arrays.asList(new String[] {});
+    List<String> s = ListExamples.filter(input, strCheck);
+
+    Assert.assertArrayEquals(output.toArray(), s.toArray());
+  }
+
+  @Test(timeout = 100)
+  public void testMany() {
+    List<String> input = Arrays.asList(new String[] { "tired422", "tired422", "tired422" });
+    List<String> output = Arrays.asList(new String[] { "tired422", "tired422", "tired422" });
+    List<String> s = ListExamples.filter(input, strCheck);
+
+    Assert.assertArrayEquals(output.toArray(), s.toArray());
+  }
+
+  @Test(timeout = 100)
+  public void testNullOutput() {
+    List<String> input = Arrays.asList(new String[] { "tired42", "tired423", "tired420" });
+    List<String> output = Arrays.asList(new String[] {});
+    List<String> s = ListExamples.filter(input, strCheck);
+
+    Assert.assertArrayEquals(output.toArray(), s.toArray());
+  }
+
+  @Test(timeout = 100)
+  public void testMerge() {
+    List<String> input1 = new ArrayList<>();
+    List<String> input2 = new ArrayList<>();
+    List<String> expected = new ArrayList<>();
+    input1.add("ti");
+    input1.add("re");
+    input2.add("d");
+    input2.add("422");
+    expected.add("ti");
+    expected.add("d");
+    expected.add("re");
+    expected.add("422");
+    assertEquals(expected, ListExamples.merge(input1, input2));
+  }
+
+  @Test(timeout = 100)
+  public void testMergeMany() {
+    List<String> input1 = new ArrayList<>();
+    List<String> input2 = new ArrayList<>();
+    List<String> expected = new ArrayList<>();
+    input1.add("ti");
+    input1.add("re");
+    input2.add("ti");
+    input2.add("re");
+    expected.add("ti");
+    expected.add("ti");
+    expected.add("re");
+    expected.add("re");
+    assertEquals(expected, ListExamples.merge(input1, input2));
+  }
+
+  @Test(timeout = 100)
+  public void testMergeNone() {
+    List<String> input1 = new ArrayList<>();
+    List<String> input2 = new ArrayList<>();
+    List<String> expected = new ArrayList<>();
+    assertEquals(expected, ListExamples.merge(input1, input2));
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,44 @@
 # Create your grading script here
-
 set -e
-
 rm -rf student-submission
 git clone $1 student-submission
+cd student-submission
 
+set -e
+if [[ -e ListExamples.java ]]
+then
+    echo "file found"
+else
+    echo "file not found, check formatting or naming"
+    exit 1
+fi
+cd .. 
+set +e
+javac -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" *.java 2> stderr.txt
+
+if [[ $? -ne 0 ]]
+then
+    echo "compiler error"
+    cat stderr.txt 
+    exit 1
+else
+    echo "successfully compiled"
+fi
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples > output.txt
+grep "Tests run:" < output.txt > results.txt
+PASSED=`wc -l results.txt | grep -o '[0-9]'`
+VARS=(`grep -Eo '[0-9]{1,2}' < results.txt`)
+
+if [[ ${VARS[0]} -ne ${VARS[1]} ]]
+then
+    echo "error"
+    echo "$((${VARS[0]}-${VARS[1]}))/7 tests passed."
+else
+    echo "All tests passed."
+fi
+cat output.txt
+
+
+
+    
